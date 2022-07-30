c6e2994fcbca88359814c975512ee32020ad1b98
acabot01 <108638645+acabot01@users.noreply.github.com>
2022-07-30 09:08:58 +0700

    Add files via upload

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = `*SAYA NYARI ESCE CUMA DI GITHUB BANH*\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/AyGemuy/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+let str = `*MAU CARI SC BOT NYA? SC PRIVATE KALAU MAU*\nSilahkan BELI HUBUNGI OWNER KAMI ,Terimakasih`
+conn.sendHydrated(m.chat, str, wm, pepe, 'http://wa.me//6283167581315', 'WA OWNER', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
